1ad5bc50b81a860046aa0b9be82b498ec8ebc7ee
Your Name <chamidilshan55@gmail.com>
2023-05-02 10:23:05 +0530

    started project

---
+++ android/app/src/main/kotlin/com/example/amazoon_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amazoon_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:amazoon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings){
+  switch(routeSettings.name){
+    case AuthScreen.routeName: return MaterialPageRoute(
+      settings: routeSettings,
+        builder: (_) => AuthScreen()
+    );
+    default: return MaterialPageRoute(
+  settings: routeSettings,
+  builder: (_) => Scaffold(
+    body: Center(
+      child: Text(
+        'Screen does not exist'
+      ),
+    ),
+  )
+  );
+  }
+}

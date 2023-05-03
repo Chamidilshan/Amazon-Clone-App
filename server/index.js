d5c395ddd1a62eacbd48266b57018068946b69e8
Your Name <chamidilshan55@gmail.com>
2023-05-03 20:45:04 +0530

    added auth page authentication for backend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require('./routes/auth');
+require('dotenv').config();
 
 const PORT = 3000;
 const app = express();
-const DB = "mongodb+srv://chamidilshan:Chamika5$@cluster0.cxc29kz.mongodb.net/?retryWrites=true&w=majority";
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB = "mongodb+srv://chamidilshan:" + DB_PASSWORD + "@cluster0.cxc29kz.mongodb.net/?retryWrites=true&w=majority";
 
+app.use(express.json());
 app.use(authRouter);
- 
+  
 mongoose.connect(DB).then(()=>{
-    console.log('connected to database succefully');
+    console.log('connected to database sucessfully');
 }).catch((e)=>{
     console.log(e);
 })

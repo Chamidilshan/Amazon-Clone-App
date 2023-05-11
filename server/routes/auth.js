3de2f61fc4cbb3bd1bf2c7740bc2cfbf2cff9dea
Your Name <chamidilshan55@gmail.com>
2023-05-11 15:48:17 +0530

    added signin route with jsonwebtoken

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
-
+const jwt = require("jsonwebtoken"); 
 const authRouter = express.Router(); 
 
 //SignUp route
@@ -30,6 +30,30 @@ authRouter.post('/api/signup', async (req, res) =>{
     };
 
 
-})
+});
+
+
+//SignIn route
+authRouter.post('/api/signin', async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(400).json({msg: 'User with this email does not exist'});
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({msg: 'Incorrect password'});
+        }
+
+        const token = jwt.sign({id: user._id}, "passwordKey");
+        res.json({token, ...user._doc})
+
+    } catch (e){
+        res.status(500).json({error: e.message});
+    }
+});
 
 module.exports = authRouter;  
